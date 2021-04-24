7fe24317380d7794e7b7fc4a0f0dfc92f8d417c2
Joseph Kilgore <joey.a.kilgore@gmail.com>
2021-04-23 22:07:07 -0400

    parsing the en-note content

--- noteParse.py
+++ noteParse.py
@@ -1,9 +1,9 @@
 import xml.etree.ElementTree as ET
 
 def recursivePrintTree(prefix, node):
-    print(prefix + node.tag)
+    print(prefix + "|-->" + node.tag)
     for child in node:
-        recursivePrintTree(prefix+"  ", child)
+        recursivePrintTree(prefix+"    ", child)
 
 def printTree(fileName):
     tree = ET.parse(fileName)
@@ -13,3 +13,13 @@ def printTree(fileName):
 
 printTree('./sample-notes/Just Text.enex')
 printTree('./sample-notes/Text and Image.enex')
+
+contentParse = ET.fromstring("""<en-note>
+        <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+        scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
+        electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of 
+        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+        Aldus PageMaker including versions of Lorem Ipsum.</div>
+      </en-note>""")
+print(contentParse)
