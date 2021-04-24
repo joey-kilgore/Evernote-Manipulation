88322d41f94fff00a7e1896c4b6f2c86caf38fd8
Joey Kilgore <joey.a.kilgore@gmail.com>
2021-04-23 23:01:23 -0400

    parse file to note object

---
+++ note.py
@@ -0,0 +1,17 @@
+class Note:
+    title = ""
+    created = ""
+    updated = ""
+    author = ""
+    contentTree = ""
+    resources = ""
+
+    def __init__(self):
+        self.title = "new note"
+
+    def printBasicStats(self):
+        print("TITLE: "+self.title)
+        print("CREATED: "+self.created)
+        print("AUTHOR: "+self.author)
+        print("DIVS: "+str(len(self.contentTree.findall('div'))))
+        print("RESOURCES: "+str(len(self.resources)))
--- noteParse.py
+++ noteParse.py
@@ -1,4 +1,32 @@
 import xml.etree.ElementTree as ET
+import note
+
+def parseNote(fileName):
+    tree = ET.parse(fileName)
+    enExport = tree.getroot()
+    noteTree = enExport[0]
+    title = noteTree.findall('title')[0].text
+    author = noteTree.findall('note-attributes')[0].findall('author')[0].text
+    created = noteTree.findall('created')[0].text
+    updated = noteTree.findall('updated')[0].text
+    
+    contentStr = noteTree.findall('content')[0].text
+    contentStr = contentStr.replace("""<?xml version="1.0" encoding="UTF-8" standalone="no"?>""", "")
+    contentStr = contentStr.replace("""<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">""","")
+    contentTree = ET.fromstring(contentStr)
+    
+    resourceListTree = noteTree.findall('resource')
+
+    noteObj = note.Note()
+    noteObj.author = author
+    noteObj.title = title
+    noteObj.created = created
+    noteObj.updated = updated
+    noteObj.contentTree = contentTree
+    noteObj.resources = resourceListTree
+
+    return noteObj
+
 
 def recursivePrintTree(prefix, node):
     print(prefix + "|-->" + node.tag)
@@ -22,4 +50,7 @@ contentParse = ET.fromstring("""<en-note>
         Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
         Aldus PageMaker including versions of Lorem Ipsum.</div>
       </en-note>""")
-print(contentParse)
+recursivePrintTree("",contentParse)
+
+parsedNote = parseNote('./sample-notes/Just Text.enex')
+parsedNote.printBasicStats()
